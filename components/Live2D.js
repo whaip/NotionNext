ae35034e81826180b698e98af38e40af85c3ac69
whp <128711613+whaip@users.noreply.github.com>
2026-01-22 21:35:03 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -47,7 +47,7 @@ export default function Live2D() {
   return (
     <canvas
       id='live2d'
-      width='300'
+      width='350'
       height='300'
       onClick={handleClick}
       className='cursor-grab'
